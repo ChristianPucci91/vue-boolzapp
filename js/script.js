c2b0786e4ca65486fec33a9c526877e1be4775c4
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-23 12:33:46 +0100

    Milestone 2, creata funzione per selezionare utente attivo al click con rispettivi dati dell'array selezionato

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// Per oggi saremo sulla milestone2, chat differenziate:
+// cliccando sulla chat vedo quella corrispondente;
+// con header con dati relativi a chat attiva;
+// relativa chat in elenco rimane selezionata
+
+
+var app = new Vue({
+  el: "#root",
+  data: {
+    selected:0,
+    users : [
+      {
+        name:"Michele",
+        img:"img/avatar_1.jpg",
+        chat: [
+         {
+           time:"16:28",
+           msgSent:"ciao Michele come stai?",
+         },
+         {
+           time:"16:30",
+           msgRcv:"tutto bene,grazie",
+         }
+       ]
+      },
+      {
+        name:"Giovanni",
+        img:"img/avatar_2.jpg",
+        chat: [
+         {
+           time:"16:28",
+           msgSent:"ciao Giovanni come stai?",
+         },
+         {
+           time:"16:40",
+           msgRcv:"tutto bene,grazie",
+         }
+       ]
+      },
+      {
+        name:"Fabio",
+        img:"img/avatar_3.jpg",
+        chat: [
+         {
+           time:"16:28",
+           msgSent:"ciao Fabio come stai?",
+         },
+         {
+           time:"16:50",
+           msgRcv:"tutto bene,grazie",
+         }
+       ]
+      },
+      {
+        name:"David",
+        img:"img/avatar_4.jpg",
+        chat: [
+         {
+           time:"16:28",
+           msgSent:"ciao David come stai?",
+         },
+         {
+           time:"16:10",
+           msgRcv:"tutto bene,grazie",
+         }
+       ]
+      },
+   ]
+  },
+  methods: {
+    selUser:function (index) {
+      this.selected = index;
+    }
+  }
+
+})//// FUNZIONAAA
